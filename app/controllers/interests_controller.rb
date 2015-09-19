e773c04ca7de13d84fc049a82be263e83a40f317
Peter He <hexinpeter@gmail.com>
2015-09-20 02:23:35 +1000

    minor change to interest notify message

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -2,7 +2,7 @@ class InterestsController < ApplicationController
 
   def create
     @interest = Interest.create(interest_params)
-    redirect_to root_path, notice: 'Thank you for your interests!'
+    redirect_to root_path, notice: "Thank you for your interests! We'll definitely let you know when it's ready :D "
   end
 
   private
