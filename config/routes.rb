0ca59fa01179a26b03bed03a2a8c5ab8423c831a
Peter He <hexinpeter@gmail.com>
2015-09-19 12:15:31 +1000

    Links to interest page added contact email added to the footer better organisation of shared CSS code

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'home/index'
-  get 'home/interest'
+  get 'home/interest', as: 'interest'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
