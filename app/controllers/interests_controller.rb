b1f51b5652b227472b40565f90d4cdfdcfe8ffbc
Peter He <hexinpeter@gmail.com>
2015-09-20 16:58:18 +1000

    minor change

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -2,7 +2,7 @@ class InterestsController < ApplicationController
 
   def create
     @interest = Interest.create(interest_params)
-    redirect_to root_path, notice: "Thank you for your interests! We'll definitely let you know when it's ready :D "
+    redirect_to root_path, notice: "Thank you for your interests! We'll definitely let you know when the site is ready :D "
   end
 
   private
