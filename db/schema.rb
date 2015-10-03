935407a5d9e19f51d6010a4b77eaa1f8f6acb100
Peter He <hexinpeter@gmail.com>
2015-10-03 15:29:10 +1000

    schema commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001122734) do
+ActiveRecord::Schema.define(version: 20151003050854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.string "state"
+  end
 
   create_table "interests", force: :cascade do |t|
     t.string   "email"
@@ -22,6 +27,20 @@ ActiveRecord::Schema.define(version: 20151001122734) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "school_id"
+    t.integer  "user_id"
+    t.integer  "city_id"
+    t.string   "phone"
+    t.string   "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "profiles", ["city_id"], name: "index_profiles_on_city_id", using: :btree
+  add_index "profiles", ["school_id"], name: "index_profiles_on_school_id", using: :btree
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "schools", force: :cascade do |t|
     t.string "name"
@@ -47,4 +66,6 @@ ActiveRecord::Schema.define(version: 20151001122734) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "profiles", "schools"
+  add_foreign_key "profiles", "users"
 end
