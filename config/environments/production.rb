2eb1462c146dd0318429fb8265f45a5b0ec0a5e3
Peter He <hexinpeter@gmail.com>
2015-10-08 05:56:46 +1100

    email url bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # for Devise
-  config.action_mailer.default_url_options = { host: 'heysenior.com' }
+  config.action_mailer.default_url_options = { host: 'www.heysenior.com' }
 end
