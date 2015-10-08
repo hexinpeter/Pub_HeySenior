b57ca8c69139de31f0f5141a04e3a3b2fd02c765
Peter He <hexinpeter@gmail.com>
2015-10-08 16:35:58 +1100

    s3 path env fix

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -13,7 +13,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "#{ENV['server_type']}_uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{Figaro.env.server_type}_uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
