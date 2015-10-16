7720b7ebee5e6b26bbe373ff7704f1880b876226
Peter He <hexinpeter@gmail.com>
2015-10-16 16:20:49 +1100

    MessageMailer

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
 
     if (@message.sender == current_user) and (@message.save)
+      MessageMailer.send_email(@message).deliver_later
       redirect_to :back, notice: 'Message was successfully created.'
     else
       redirect_to :back, alert: 'Message failed to send.'
---
+++ test/mailers/previews/message_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/message_mailer
+class MessageMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/message_mailer/send_email
+  def send_email
+    MessageMailer.send_email
+  end
+
+end
